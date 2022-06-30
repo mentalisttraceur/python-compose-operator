b316ed663ac43385ecd4cfabd7a2862ffc7b9be6
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2022-06-30 02:39:14 -0400

    0.3.0 release

--- no_positional_only_arguments.py
+++ no_positional_only_arguments.py
@@ -4,7 +4,7 @@
 """Operator overload for function composition."""
 
 __all__ = ('composable', 'composable_constructor', 'composable_instances')
-__version__ = '0.2.3'
+__version__ = '0.3.0'
 
 
 from compose import sacompose as _compose
