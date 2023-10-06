c842893b73dbe49aa961dba6e06393dc9a3e10ec
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2023-10-06 19:08:30 +0000

    Cut release v0.5.0

--- compose_operator.py
+++ compose_operator.py
@@ -4,7 +4,7 @@
 """Operator overload for function composition."""
 
 __all__ = ('composable', 'composable_constructor', 'composable_instances')
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 
 from copy import deepcopy as _deepcopy
