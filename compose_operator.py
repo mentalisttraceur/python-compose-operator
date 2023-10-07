3099ef7cc07c8a8427ea16642840d4543748627d
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2023-10-07 01:41:21 +0000

    Bump version to publish README updates

--- compose_operator.py
+++ compose_operator.py
@@ -4,7 +4,7 @@
 """Operator overload for function composition."""
 
 __all__ = ('composable', 'composable_constructor', 'composable_instances')
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 
 
 from copy import deepcopy as _deepcopy
